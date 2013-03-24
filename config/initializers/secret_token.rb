01d4a1b64d3bf68fea103b29bcee63e1ae8e4633
Orion Kostival <okostiva@mines.edu>
2013-03-23 23:12:16 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FotoShare::Application.config.secret_token = 'f6466efa7f573c61cae7146c345eb08a212da15a7b42a304c344251d603ba992a5abb6ef3e61b0dfa00a1ffe6f883ddf87af379f397b50b110e5bd0cba0e3d88'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+FotoShare::Application.config.session_store :cookie_store, key: '_FotoShare_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# FotoShare::Application.config.session_store :active_record_store
