89ad7a8db196325a13cfcbfc1f1abb959802f3f7
Orion Kostival <okostiva@mines.edu>
2013-04-20 13:34:40 -0600

    Modified file size requirements

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -5,7 +5,7 @@ class Photo < ActiveRecord::Base
 
 	validates_attachment :picture, :presence => true,
 		:content_type => { :content_type => ["image/jpg","image/jpeg","image/png","image/gif"], :message => "must be a jpg, png or gif image." }, 
-		:size => { :in => 0..1.megabytes, :message => "must be less than 1 megabyte" }
+		:size => { :in => 0..2.megabytes, :message => "must be less than 1 megabyte" }
 
 	has_attached_file :picture, :path => "cs446/kostival/#{Rails.env}:url",
 		:styles => { :small => "100x100>", :medium => "300x300>", :large => "500x500>" }, :default_url => "/images/photos/upload.png"
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
 	validates :first_name, presence: true
 	validates :last_name, presence: true
 	validates_attachment :avatar, :content_type => { :content_type => ["image/jpg","image/jpeg","image/png","image/gif"], :message => "must be a jpg, png or gif image." }, 
-		:size => { :in => 0..1.megabytes, :message => "must be less than 1 megabyte" }
+		:size => { :in => 0..2.megabytes, :message => "must be less than 1 megabyte" }
 	has_secure_password
 
 	has_attached_file :avatar, :path => "cs446/kostival/#{Rails.env}:url",
